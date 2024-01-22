0c10c89efde618d3bf19b4e34a4935a06bbdc423
khach <khachgevorgyan94@gmail.com>
2024-01-22 21:38:15 +0400

    test

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import {IUserData} from "./models/user.model";
 import ReactDatePicker from "react-datepicker";
 import './assets/style/index.scss'
 import axios, {AxiosResponse} from "axios";
+import {AuthSlice, setTokenData} from "./store/redusers/authSlice";
+import {useSelector} from "react-redux";
+import {useAppSelector} from "./hooks/redux";
 
 /**
  *  TypeScript projkect@ install anelu hamar reacti mijocov  grum eqn hetevyal hraman@
@@ -25,33 +28,46 @@ interface IProductData {
 }
 
 function App() {
-    const [productList,setProductList] = useState<IProductData[]>([])
 
-
-    useEffect(() => {
-        getProductList()
-    }, [])
-
-
-    const getProductList = async () => {
-        const result = await axios.get<null,AxiosResponse<IProductData[]>>('https://crudcrud.com/api/9e6dd515c4cb4eac9edac88b7b70617d/productH')
-        if (result) {
-            console.log(result.data)
-            setProductList(result.data)
-        }
-    }
+    // const tokenData = useAppSelector(state=>state.authSlice)
+    // const [productList,setProductList] = useState<IProductData[]>([])
+    //
+    //
+    // useEffect(() => {
+    //     getProductList()
+    // }, [])
+    //
+    //
+    // const getProductList = async () => {
+    //     const result = await axios.get<null,AxiosResponse<IProductData[]>>('https://crudcrud.com/api/9e6dd515c4cb4eac9edac88b7b70617d/productH')
+    //     if (result) {
+    //         console.log(result.data)
+    //         setProductList(result.data)
+    //     }
+    // }
 
     // function GET(url:string, data:any){
     //     axios.get(url, data)
     // }
 
+    const handleClick = ()=>{
+        setTokenData('468sdf90g-78s-d98gus[9t')
+
+    }
+
+    useEffect(() => {
+
+    }, []);
+
     return (
         <div>
-            {productList.map((item,index)=>{
-                return <div>
-                    {item.name}
-                </div>
-            })}
+            {/*{productList.map((item,index)=>{*/}
+            {/*    return <div>*/}
+            {/*        {item.name}*/}
+            {/*    </div>*/}
+            {/*})}*/}
+
+            <button >Set Token</button>
         </div>
     );
 }
